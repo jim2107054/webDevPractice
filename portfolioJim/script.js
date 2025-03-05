6af740720e55e1b0029a30d13d8359ff631299af
jim2107054 <mdjahid205hasan@gmail.com>
2025-03-05 19:30:35 +0600

    edit the code

--- portfolioJim/script.js
+++ portfolioJim/script.js
@@ -1,10 +1,16 @@
 const aboutBtn = document.querySelectorAll('.about-btn');
 
-aboutBtn.forEach((btn)=>{
+aboutBtn.forEach((btn,idx)=>{
     btn.addEventListener('click',()=>{
+        const aboutDetail = document.querySelectorAll('.about-detail');
         aboutBtn.forEach(btn=>{
             btn.classList.remove('active');
         });
         btn.classList.add('active');
+
+        aboutDetail.forEach(detail=>{
+            detail.classList.remove('active');
+        });
+        aboutDetail[idx].classList.add('active');
     });
 });
