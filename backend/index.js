3470381e25c30c631a03f4c6c4e8e91c56414fbf
jim2107054 <mdjahid205hasan@gmail.com>
2025-04-17 23:18:44 +0600

    level2 done

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+import http from 'http';
+
+const port = 5000;
+
+const server = http.createServer((req,res)=>{
+    if(req.url === '/'){
+        res.end("<h1>Welcome to the home page</h1>")
+    }
+    else if(req.url === '/about'){
+        res.end("<h1>About Page.</h1>")
+    }
+    else{
+        res.end("<h1>404 Page Not Found</h1>");
+    }
+})
+
+server.listen(port);
