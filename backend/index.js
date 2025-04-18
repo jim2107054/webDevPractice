67e1fc98597f1fa256b3e01ec2867ece3e276d46
jim2107054 <mdjahid205hasan@gmail.com>
2025-04-18 10:12:14 +0600

    start learning express

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,42 @@
-import http from 'http';
+import express from 'express';
+const app = express();//call express. here app is an instance of express
 
-const port = 5000;
+const port = 5000;//port number
 
-const server = http.createServer((req,res)=>{
-    if(req.url === '/'){
-        res.end("<h1>Welcome to the home page</h1>")
-    }
-    else if(req.url === '/about'){
-        res.end("<h1>About Page.</h1>")
-    }
-    else{
-        res.end("<h1>404 Page Not Found</h1>");
-    }
+app.get("/",(req,res)=>{
+    res.send("<h1>Welcome to the home page</h1>")
 })
 
-server.listen(port);
+app.get("/about",(req,res)=>{
+    res.end("<p>about page, tell me about yourself. Hi I'm Jim</p>")
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
+
+
+
+ 
+
+
+
+
+
+// import http from 'http';
+
+// const port = 5000;
+
+// const server = http.createServer((req,res)=>{
+//     if(req.url === '/'){
+//         res.end("<h1>Welcome to the home page</h1>")
+//     }
+//     else if(req.url === '/about'){
+//         res.end("<h1>About Page.</h1>")
+//     }
+//     else{
+//         res.end("<h1>404 Page Not Found</h1>");
+//     }
+// })
+
+// server.listen(port);
