1d7738a5012d283d68378f32331fd36b7f6224db
jim2107054 <mdjahid205hasan@gmail.com>
2025-04-21 12:47:19 +0600

    crud operation done

--- DataBase/index.js
+++ DataBase/index.js
@@ -85,6 +85,38 @@ app.post('/users/find', async (req,res)=>{
     }
 })
 
+app.put('/users/update/:id', async (req,res)=>{
+    const {id} = req.params;
+    const {name,age,email} = req.body;
+    try{
+        const existUser = await User.findById(id);
+        if(!existUser){
+            return res.status(404).json({message:"User not found"})
+        }
+    const updatedUser = await User.findByIdAndUpdate(id,{name,age,email},{new:true});
+        res.status(200).json({message:"User updated successfully", updatedUser})
+    }
+    catch(error){
+        console.log("Error occured",error);
+        res.status(500).json({message:"Internal server error",error})
+    }
+})
+
+app.delete('/users/delete/:id', async (req,res)=>{
+    const {id} = req.params;
+    try{
+        const existUser = await User.findById(id);
+        if(!existUser){
+            return res.status(404).json({message:"User not found"})
+        }
+        await User.findByIdAndDelete(id);
+        res.status(200).json({message:"User deleted successfully"})
+    }
+    catch(error){
+        console.log("Error occured",error);
+        res.status(500).json({message:"Internal server error",error})
+    }
+})
 
 app.listen(port,()=>{
     connectDB();
