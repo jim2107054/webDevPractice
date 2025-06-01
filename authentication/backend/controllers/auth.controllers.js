7ed185093aab5c7592b4fbfcbeba3e031aa16d5f
jim2107054 <mdjahid205hasan@gmail.com>
2025-06-01 20:16:46 +0600

    login done

--- authentication/backend/controllers/auth.controllers.js
+++ authentication/backend/controllers/auth.controllers.js
@@ -1,61 +1,98 @@
-import generateToken from '../config/token.js';
-import User from './../models/User.model.js';
-import bcrypt from 'bcryptjs';
+import generateToken from "../config/token.js";
+import User from "./../models/User.model.js";
+import bcrypt from "bcryptjs";
 
 export const SignUp = async (req, res) => {
-    
-    try{
-        const {firstName, lastName, email, password} = req.body;
-        // check if all fields are provided
-        if (!firstName || !lastName || !email || !password) {
-            return res.status(400).json({ message: "All fields are required" });
-        }
-
-        //if user already exists
-        const existUser = await User.findOne({email});
-        if (existUser) {
-            return res.status(400).json({ message: "User already exists" });
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // create and save new user
-        // const newUser = new User({
-        //     firstName,
-        //     lastName,
-        //     email,
-        //     password: hashedPassword,
-        // });
-        // await newUser.save();
-        // Create the user and return relevant info (excluding password) in response
-        const createdUser = await User.create({
-            firstName,
-            lastName,
-            email,
-            password: hashedPassword,
-        });
-
-        const token = generateToken(createdUser._id);
-        // Set the token in the response header
-        res.cookie("token", token, {
-            httpOnly: true,// Prevents client-side JavaScript from accessing the cookie
-            secure: process.env.NODE_ENV === "production", // Use secure cookies in production
-            sameSite: "strict", // Helps prevent CSRF attacks
-            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-        });
-
-        res.status(201).json({ 
-            message: "User created successfully",
-            user: {
-                id: createdUser._id,
-                firstName: createdUser.firstName,
-                lastName: createdUser.lastName,
-                email: createdUser.email
-            }
-        });
+  try {
+    const { firstName, lastName, email, password } = req.body;
+    // check if all fields are provided
+    if (!firstName || !lastName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
     }
-    catch (error) {
-        console.error("Error during signup:", error);
-        res.status(500).json({ message: "Internal server error" });
+
+    //if user already exists
+    const existUser = await User.findOne({ email });
+    if (existUser) {
+      return res.status(400).json({ message: "User already exists" });
     }
-}
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // create and save new user
+    // const newUser = new User({
+    //     firstName,
+    //     lastName,
+    //     email,
+    //     password: hashedPassword,
+    // });
+    // await newUser.save();
+    // Create the user and return relevant info (excluding password) in response
+    const createdUser = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = generateToken(createdUser._id);
+    // Set the token in the response header
+    res.cookie("token", token, {
+      httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
+      secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+      sameSite: "strict", // Helps prevent CSRF attacks
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+
+    res.status(201).json({
+      message: "User created successfully",
+      user: {
+        id: createdUser._id,
+        firstName: createdUser.firstName,
+        lastName: createdUser.lastName,
+        email: createdUser.email,
+      },
+    });
+  } catch (error) {
+    console.error("Error during signup:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const Login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    //check if the user exists
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return res.status(400).json({ message: "User does not exist" });
+    }
+
+    //check if the password is correct
+    const match = await bcrypt.compare(password, existingUser.password);
+    if (!match) {
+      return res.status(400).json({ message: "Invalid Password" });
+    }
+
+    // Generate token
+    const token = generateToken(existingUser._id);
+    // Set the token in the response header
+    res.cookie("token", token, {
+      httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
+      secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+      sameSite: "strict", // Helps prevent CSRF attacks
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+
+    res.status(200).json({
+      message: "Login successful",
+      user:{
+        firstName: existingUser.firstName,
+        lastName: existingUser.lastName,
+        email: existingUser.email,
+      }
+    });
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- authentication/backend/routes/auth.route.js
+++ authentication/backend/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express, { Router } from 'express';
-import { SignUp } from '../controllers/auth.controllers.js';
+import { Login, SignUp } from '../controllers/auth.controllers.js';
 
 const authRouter = express(Router());
 
 authRouter.post("/signup",SignUp)
+authRouter.post("/login",Login)
 
 export default authRouter;
